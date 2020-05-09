1ae2c75e731ac5f2c100cc83647fae7b79abd0ef
Rajath <rvgofmys@gmail.com>
2020-05-09 12:37:53 +0530

    first oush

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollbutton : Button = findViewById(R.id.roll_button)
+        rollbutton.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice(){
+        Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
     }
 }
