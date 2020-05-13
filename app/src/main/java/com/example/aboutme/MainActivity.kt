bcfc7ed6d229c5bc0106a52b1b5c0ab411490ef7
Rajath <rvgofmys@gmail.com>
2020-05-13 13:10:13 +0530

    projectname

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.aboutme
+
 import java.util.*;
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,46 +12,48 @@ import androidx.core.text.isDigitsOnly
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var diceImage : ImageView
-    lateinit var diceImage2 : ImageView
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollbutton : Button = findViewById(R.id.roll_button)
-        rollbutton.setOnClickListener{rollDice(); rollDice2()}
+        val rollbutton: Button = findViewById(R.id.roll_button)
+        rollbutton.setOnClickListener { rollDice(); rollDice2() }
     }
-private fun rollDice2(){
-    val randomInt = (1..6).random()
 
-    diceImage2  = findViewById(R.id.diceImage2)
+    private fun rollDice2() {
+        val randomInt = (1..6).random()
 
-    var drawableresource = when(randomInt){
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
+        diceImage2 = findViewById(R.id.diceImage2)
+
+        var drawableresource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage2.setImageResource(drawableresource)
+
     }
-    diceImage2.setImageResource(drawableresource)
 
-}
-    private fun rollDice(){
-      //  Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
+    private fun rollDice() {
+        //  Toast.makeText(this,"button clicked", Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
 
 
-         diceImage  = findViewById(R.id.diceImage)
+        diceImage = findViewById(R.id.diceImage)
 
-        var drawableresource = when(randomInt){
-    1 -> R.drawable.dice_1
-    2 -> R.drawable.dice_2
-    3 -> R.drawable.dice_3
-    4 -> R.drawable.dice_4
-    5 -> R.drawable.dice_5
-    else -> R.drawable.dice_6
-}
+        var drawableresource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
         diceImage.setImageResource(drawableresource)
 
     }
